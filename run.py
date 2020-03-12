77c3f1b072f2c9f4046e3df5e585ff8b6d11a70d
Pengyu Cheng <pcheng11@illinois.edu>
2020-03-12 17:41:46 -0400

    worker list refine

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from src import app
 from src import celery
-
 if __name__ == "__main__":
     # app = create_app(debug=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0',port=5000)
