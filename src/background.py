89fe230fadba2aca278518928c931270c3e4d48c
Pengyu Cheng <pcheng11@illinois.edu>
2020-03-19 16:28:23 -0400

    refactor code

--- src/background.py
+++ src/background.py
@@ -3,7 +3,7 @@ from datetime import timedelta, datetime
 
 from src.model import AutoScalingConfig
 from src import background_task, cw, app
-from background_task.task import periodic_task
+from celery.task import periodic_task
 from celery import Celery
 import numpy as np
 import random
@@ -13,7 +13,59 @@ from src.cpu import get_single_instance_cpu_util
 from src.worker import celery_create_worker, random_destroy_worker
 
 
+@periodic_task(run_every=timedelta(seconds=60))
+def record_serving_instances_avg_cpu_util():
+    inservice_instances_id, num_inserivce_instances = get_serving_instances()
 
+    response = cw.put_metric_data(
+        Namespace='AWS/EC2',
+        MetricData=[
+            {
+                'MetricName': 'numWorkers30',
+                'Timestamp': datetime.now(),
+                'Value': num_inserivce_instances,
+                'Dimensions': [
+                    {
+                        'Name': 'InstanceId',
+                        'Value': 'i-078f69c8c9c0097d6'
+                    },
+                ],
+                'StorageResolution': 60,
+                'Unit': 'Count'
+            },
+        ]
+    )
+    print('number of healthy instances: ' + str(len(inservice_instances_id)))
+
+    cpu_stats_list = []
+    avg_cpu_util = 0
+
+    if len(inservice_instances_id) != 0:
+        for instance_id in inservice_instances_id:
+            cpu_stats = get_single_instance_cpu_util(instance_id, 2)
+            if len(cpu_stats) != 0:
+                cpu_stats_list.append(np.mean(cpu_stats))
+        if len(cpu_stats_list) != 0:
+            avg_cpu_util = np.mean(cpu_stats_list)
+
+    response = cw.put_metric_data(
+        Namespace='AWS/EC2',
+        MetricData=[
+            {
+                'MetricName': 'avgCPUutil',
+                'Timestamp': datetime.now(),
+                'Value': avg_cpu_util,
+                'Dimensions': [
+                    {
+                        'Name': 'InstanceId',
+                        'Value': 'i-078f69c8c9c0097d6'
+                    },
+                ],
+                'StorageResolution': 60,
+                'Unit': 'Count'
+            },
+        ]
+    )
 
 
 @periodic_task(run_every=timedelta(seconds=60))
